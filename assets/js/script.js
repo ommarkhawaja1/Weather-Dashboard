bfe890819411dde7405dae1466ffb11912b25455
Ommar Khawaja <ommarkhawaja1@gmail.com>
2022-06-14 14:42:47 -0400

    display weather, clear input on submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,15 +19,27 @@ var weatherApiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&l
 
 var weatherForm = document.querySelector("#weather-form");
 var cityInput = document.querySelector("#city");
-// var weatherContainer = document.querySelector("#weather-container");
 
 
-var searchCity = function (event) {
+var formSubmitHandler = function (event) {
   event.preventDefault();
 
   // get value from input element
-  var city = cityInput.value.trim();
+  var city = cityInput.value.trim()
 
+  if (city) {
+      searchCity(city);
+
+      // clear old content
+      cityInput.value = "";
+  } else {
+      alert("Please enter a city name")
+  }
+  console.log(event);
+};
+
+
+var searchCity = function (city) {
   // format the github api url
   var locationApiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=e0090736d1a7d7d3d3989801cd196bfa"
 
@@ -39,13 +51,15 @@ var searchCity = function (event) {
   .then(function (data) {
     var lat = data[0].lat;
     var lon = data[0].lon;
+    console.log(lat, lon)
     getCityWeather(lat, lon);
   });
 
   
 function getCityWeather (lat, lon) {
   // format the github api url
-  var weatherApiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" +lon + "&exclude=minutely,hourly&appid=e0090736d1a7d7d3d3989801cd196bfa"
+  var weatherApiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" +lon + "&units=imperial&exclude=minutely,hourly&appid=e0090736d1a7d7d3d3989801cd196bfa"
+  console.log(weatherApiUrl)
   // make a request to the url
   fetch(weatherApiUrl)
   .then(function (response) {
@@ -55,8 +69,49 @@ function getCityWeather (lat, lon) {
       var currentData = data.current;
       var dailyData = data.daily;
       console.log(currentData, dailyData)
+      displayWeather(currentData, dailyData)
     });
   }
 };
 
-weatherForm.addEventListener("submit", searchCity);
+function displayWeather (currentData, dailyData) {
+ //create children for current info card
+ var cardBodyEl = $("#weather-container")
+  
+
+cardBodyEl.append(cityInput.value.trim())
+
+ //create and append icon
+ var iconCode = currentData.weather[0].icon;
+ var iconLocation = "./assets/images/" + iconCode + ".png";
+ var currentCloudEl = $("<img>")
+   .addClass("current-icon")
+   .attr("src", iconLocation);
+ cardBodyEl.append(currentCloudEl);
+ //create and append temperature
+ var currentTempEl = $("<p>").text("Temperature: " + currentData.temp + " °F");
+ cardBodyEl.append(currentTempEl);
+ //create and append wind speed
+ var currentWindEl = $("<p>").text("Wind: " + currentData.wind_speed + " MPH");
+ cardBodyEl.append(currentWindEl);
+ //create and append humidity
+ var currentHumidityEl = $("<p>").text(
+   "Humidity: " + currentData.humidity + "%"
+ );
+ cardBodyEl.append(currentHumidityEl);
+ //create and append uvi
+ var currentUviEl = $("<p>").text("UV Index: ");
+ var uviSpanEl = $("<span>").text(currentData.uvi);
+ if (currentData.uvi < 3.33) {
+   uviSpanEl.addClass('favorable');
+ } else if (currentData.uvi > 6.66) {
+   uviSpanEl.addClass('severe');
+ } else {
+   uviSpanEl.addClass('moderate');
+ }
+ currentUviEl.append(uviSpanEl);
+ cardBodyEl.append(currentUviEl);
+}
+
+
+weatherForm.addEventListener("submit", formSubmitHandler);
